55b20b57048d78974065f617f3850e81bc29ee25
ByeongHunKim <rkwhr912@naver.com>
2023-08-12 13:34:06 +0900

    feat: add semi rule for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -17,6 +16,10 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'semi': ['error', 'never'],
+    'arrow-parens': 'off',
+    'quotes': ['error', 'single'],
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
