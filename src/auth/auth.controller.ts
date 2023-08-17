71aa4af9d0e33a0ad5a0d580b79f57ed66427115
ByeongHunKim <rkwhr912@naver.com>
2023-08-17 17:14:59 +0900

    feat : generate auth token by using config values

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,33 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, Get } from '@nestjs/common'
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
 import { AuthTokenRequestDto } from './dto/auth.token.create.dto'
+import { ConfigService } from '@nestjs/config'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post()
   @ApiTags('auth')
   @ApiBody({ type: AuthTokenRequestDto })
-  @ApiOperation({ summary: 'generate auth token', description: 'Luniverse API에 필요한 auth token을 nodeId, apiKeyId, apiKeySecret 을 통해 발급 받습니다' })
+  @ApiOperation({ summary: 'generate auth token by using input values', description: 'Luniverse API에 필요한 auth token을 nodeId, apiKeyId, apiKeySecret 을 통해 발급 받습니다' })
   async retrieveAuthToken(
       @Body() authTokenRequestDto: AuthTokenRequestDto
   ): Promise<string> {
     const { nodeId, apiKeyId, apiKeySecret } = authTokenRequestDto
     return await this.authService.fetchAuthTokenFromLuniverseAPI(nodeId, apiKeyId, apiKeySecret)
   }
+
+  @Get()
+  @ApiTags('auth')
+  @ApiOperation({ summary: 'generate auth token by using config values', description: 'config에 세팅한 값들을 통해 빠르게 auth token을 발급 받습니다' })
+  async generateAuthToken(): Promise<string> {
+    const { nodeId, apiKeyId, apiKeySecret } = this.configService.get('nodeInfos')
+    return await this.authService.fetchAuthTokenFromLuniverseAPI(nodeId, apiKeyId, apiKeySecret)
+  }
+
 }
