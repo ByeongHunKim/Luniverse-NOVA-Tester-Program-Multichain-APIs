bccc8336db871823f1cef3c9139c2abe8095c3e9
ByeongHunKim <rkwhr912@naver.com>
2023-08-17 17:14:31 +0900

    chore : config set up

---
+++ config/index.ts
@@ -0,0 +1,23 @@
+export interface Config {
+  port: number
+  database: {
+    url: string
+  }
+  nodeInfos: {
+    nodeId: string
+    apiKeyId: string
+    apiKeySecret: string
+  }
+}
+
+export default (): Config => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    url: process.env.DATABASE_URL,
+  },
+  nodeInfos: {
+    nodeId: process.env.NODE_ID,
+    apiKeyId: process.env.API_KEY_ID,
+    apiKeySecret: process.env.API_KEY_SECRET,
+  },
+})
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { NftModule } from './nft/nft.module'
+import { ConfigModule } from '@nestjs/config'
+import config from '../config'
 
 @Module({
-  imports: [AuthModule, NftModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      load: [config],
+      isGlobal: true,
+    }),
+    AuthModule,
+    NftModule,
+  ],
 })
 export class AppModule {}
