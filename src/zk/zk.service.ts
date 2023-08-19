7a643add79b5102ffbdcb22b96f9491f9217105d
ByeongHunKim <rkwhr912@naver.com>
2023-08-19 23:16:22 +0900

    refactor: Create JSON-RPC Provider and initialize related objects in ZkService

--- src/zk/zk.service.ts
+++ src/zk/zk.service.ts
@@ -4,24 +4,33 @@ import { ethers } from 'ethers'
 
 @Injectable()
 export class ZkService {
-  private readonly contractAddress: string
-  private readonly abi: any[]
-  private readonly nodeId: string
-  private readonly endpoint: string
-  private readonly provider: ethers.JsonRpcProvider
+
+  // todo JsonProvider 를 다른 모듈로 분리해야하나..
   private readonly contract: ethers.Contract
   private readonly wallet: ethers.Wallet
+  private readonly provider: ethers.JsonRpcProvider
 
   constructor(private readonly configService: ConfigService) {
-    this.contractAddress = this.configService.get<string>('contract.address')
-    this.abi = this.configService.get<any[]>('contract.abi')
-    this.nodeId = this.configService.get<string>('nodeInfos.nodeId')
-    this.endpoint = this.configService.get<string>('contract.endpoint')
+    this.provider = this.createProvider()
+    this.contract = this.createContract()
+    this.wallet = this.createWallet()
+  }
 
-    this.provider = new ethers.JsonRpcProvider(`${this.endpoint}${this.nodeId}`)
-    this.contract = new ethers.Contract(this.contractAddress, this.abi, this.provider)
+  private createProvider(): ethers.JsonRpcProvider {
+    const nodeId = this.configService.get<string>('nodeInfos.nodeId')
+    const endpoint = this.configService.get<string>('contract.endpoint')
+    return new ethers.JsonRpcProvider(`${endpoint}${nodeId}`)
+  }
+
+  private createContract(): ethers.Contract {
+    const contractAddress = this.configService.get<string>('contract.address')
+    const abi = this.configService.get<any[]>('contract.abi')
+    return new ethers.Contract(contractAddress, abi, this.provider)
+  }
+
+  private createWallet(): ethers.Wallet {
     const signKey = this.configService.get<string>('contract.signKey')
-    this.wallet = new ethers.Wallet(signKey, this.provider)
+    return new ethers.Wallet(signKey, this.provider)
   }
 
   async createAgenda(
@@ -32,12 +41,16 @@ export class ZkService {
   ): Promise<any> {
     try {
       const contract = this.contract.connect(this.wallet) as any
-      const transaction = await contract.createAgenda(title, description, deadline, agendaId)
-      const receipt = transaction.wait()
-      console.log('receipt',receipt)
+      const transaction = await contract.createAgenda(
+        title,
+        description,
+        deadline,
+        agendaId
+      )
+      const receipt = await transaction.wait()
+      console.log('receipt', receipt)
     } catch (error) {
       console.error('Error:', error)
     }
   }
-
 }
